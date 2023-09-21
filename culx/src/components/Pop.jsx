4645356f956aa4715a50c55c4410b7e7c314a756
63brown <mutheemumbi01@gmail.com>
2023-09-21 23:52:37 +0300

    Updates on pop component

--- culx/src/components/Pop.jsx
+++ culx/src/components/Pop.jsx
@@ -1,20 +1,58 @@
-import React, { useEffect } from 'react'
+import { useEffect, useState } from 'react';
+import styled from "styled-components"
+import {Splide, SplideSlide} from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
 
 function Pop() {
 
+  const [pop, setPop] = useState([]);
+
   useEffect(() => {
     getPop();
-  },[]);
+  }, []);
 
   const getPop = async () => {
-    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.API_KEY}&number=12`)
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
     const data = await api.json();
-    console.log(data);
-  }
+    setPop(data.recipes);
+    console.log(data.recipes);
+  };
 
   return (
-    <div>Pop</div>
+    <div>
+      <Wrapper>
+        <h3>Popular Recipes</h3>
+          <Splide options={{
+             perPage: 2,
+           }}>
+            {pop.map((recipe) => {
+              return(
+                <SplideSlide>
+                  <Card>
+                    <p>{recipe.title}</p>
+                    <img src={recipe.image} alt={recipe.title} />
+                  </Card>
+                </SplideSlide>
+                )
+              })}
+            </Splide>
+          </Wrapper>
+    </div>
   )
 }
 
-export default Pop
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+  `;
+
+const Card =styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+
+  img{
+    border-radius: 2rem;
+  }
+`;
+
+export default Pop;
